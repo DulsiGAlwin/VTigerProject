e5ffdfdeb4abe4f7d2cae71fc2224677e4dd87f1
dulsi <dulsi@Dulsi-Alwin-Laptop.lan>
2022-12-06 11:20:40 -0500

    3rd commit

--- src/test/java/testNGImplementation/Testing.java
+++ src/test/java/testNGImplementation/Testing.java
@@ -1,10 +1,12 @@
 package testNGImplementation;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Testing {
 @Test
 public void testt1() {
 	System.out.println("God is Love");
+	Reporter.log("God is Great", true);
 }
 }
